3013ec3f43a2eb865cf19b0e81389be6687df701
Mikkel Davis <mikkelrd@users.noreply.github.com>
2018-08-06 10:12:35 -0600

    Ok fixed now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cssVars from './styles.json';
 import classNames from './styles.scss';
 
 console.log(cssVars); // {red: "#FF0000"}
-console.log(classNames); // {red: "#FF0000"}
+console.log(classNames); // {myDiv: "_3x9aPH9WBIFskhaiKvhWqN"}
 
 render(
 	<div className={classNames.myDiv}>asdf</div>,
