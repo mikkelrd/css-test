501d965469c6214c637a9cde6ebca9fdac04c465
Mikkel Davis <mikkelrd@users.noreply.github.com>
2018-08-06 10:09:57 -0600

    Fix var reference

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cssVars from './styles.json';
 import classNames from './styles.scss';
 
 console.log(cssVars); // {red: "#FF0000"}
-console.log(cssVars); // {red: "#FF0000"}
+console.log(classNames); // {red: "#FF0000"}
 
 render(
 	<div className={classNames.myDiv}>asdf</div>,
